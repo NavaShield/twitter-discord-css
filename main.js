917f60b2ce76d3635f3f0637870997e39b7b202e
overlord of cum <88568085+NavaShield@users.noreply.github.com>
2022-11-19 18:00:30 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         haha funny twitter discord
+// @description  i want to take screenshots which blend in
+// @author       NavaShield#6203/@NavaShieldYT
+// @version      1.0
+// @match        *://twitter.com/*
+// @run-at       document-body
+// @icon         https://raw.githubusercontent.com/ChenzIsHomo/twitter-discord-css/main/Troll.ico
+// ==/UserScript==
+
+
+(function() {
+    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+    const body = document.querySelector("body"); // sidebar style colour shit
+    body.style.backgroundColor = "rgb(54, 57, 63)" // descorb colours
+    const style = document.createElement("style");
+    const newContent = document.createTextNode(`.r-kemksi{background-color:#36393f}.r-14wv3jr,.r-1kqtdi0{border-color:#2f3136}.r-g2wdr4{background-color:#2f3136}.r-5zmot{background-color:rgba(47,49,54,.65)}`);
+    style.appendChild(newContent);
+})();
